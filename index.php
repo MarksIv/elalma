fa6cd8599c2d984a921ed42f7f71aa0b629422c1
Mark Ivanov <markivanov@Marks-MacBook-Pro-3.local>
2023-08-03 00:01:44 +0300

    add temporary galery

--- index.php
+++ index.php
@@ -396,7 +396,218 @@
             </div>
             <!-- End Title -->
 
-            <div class="row">  
+
+            <style>
+                * {
+                    box-sizing: border-box
+                }
+
+                body {
+                    font-family: Verdana, sans-serif;
+                    margin: 0
+                }
+
+                .mySlides {
+                    display: none
+                }
+
+                img {
+                    vertical-align: middle;
+                }
+
+                /* Slideshow container */
+                .slideshow-container {
+                    max-width: 1000px;
+                    position: relative;
+                    margin: auto;
+                }
+
+                /* Next & previous buttons */
+                .prev,
+                .next {
+                    cursor: pointer;
+                    position: absolute;
+                    top: 50%;
+                    width: auto;
+                    padding: 16px;
+                    margin-top: -22px;
+                    color: white;
+                    font-weight: bold;
+                    font-size: 18px;
+                    transition: 0.6s ease;
+                    border-radius: 0 3px 3px 0;
+                    user-select: none;
+                }
+
+                /* Position the "next button" to the right */
+                .next {
+                    right: 0;
+                    border-radius: 3px 0 0 3px;
+                }
+
+                /* On hover, add a black background color with a little bit see-through */
+                .prev:hover,
+                .next:hover {
+                    background-color: rgba(0, 0, 0, 0.8);
+                }
+
+                /* Caption text */
+                .text {
+                    color: #f2f2f2;
+                    font-size: 15px;
+                    padding: 8px 12px;
+                    position: absolute;
+                    bottom: 8px;
+                    width: 100%;
+                    text-align: center;
+                }
+
+                /* Number text (1/3 etc) */
+                .numbertext {
+                    color: #f2f2f2;
+                    font-size: 12px;
+                    padding: 8px 12px;
+                    position: absolute;
+                    top: 0;
+                }
+
+                /* The dots/bullets/indicators */
+                .dot {
+                    cursor: pointer;
+                    height: 15px;
+                    width: 15px;
+                    margin: 0 2px;
+                    background-color: #bbb;
+                    border-radius: 50%;
+                    display: inline-block;
+                    transition: background-color 0.6s ease;
+                }
+
+                .active,
+                .dot:hover {
+                    background-color: #717171;
+                }
+
+                /* Fading animation */
+                .fade {
+                    animation-name: fade;
+                    animation-duration: 1.5s;
+                }
+
+                @keyframes fade {
+                    from {
+                        opacity: .4
+                    }
+
+                    to {
+                        opacity: 1
+                    }
+                }
+
+                /* On smaller screens, decrease text size */
+                @media only screen and (max-width: 300px) {
+
+                    .prev,
+                    .next,
+                    .text {
+                        font-size: 11px
+                    }
+                }
+            </style>
+            </head>
+
+
+            <div class="slideshow-container">
+
+                <?php
+                $queryPhoto = "SELECT * FROM photos";
+                $resultPhoto = mysqli_query($connection, $queryPhoto);
+
+                if ($resultPhoto) {
+
+                    while ($rowPhoto = mysqli_fetch_assoc($resultPhoto)) {
+                ?>
+
+                        <div class="mySlides">
+                            <!-- <div class="numbertext">1 / 3</div> -->
+                            <img src="img/<?php echo ($rowPhoto['photoName']); ?>" style="width:100%">
+                            <!-- <div class="text">Caption Text</div> -->
+                        </div>
+
+                <?php
+                    }
+                }
+                ?>
+
+
+
+                <!-- <div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="img_snow_wide.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="img_mountains_wide.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div> -->
+
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" onclick="plusSlides(1)">❯</a>
+
+            </div>
+            <br>
+
+            <div style="text-align:center">
+                <?php
+                $i = 0;
+                $resultPhoto = mysqli_query($connection, $queryPhoto);
+                if ($resultPhoto) {
+                    while ($rowPhoto = mysqli_fetch_assoc($resultPhoto)) {
+                        $i++; 
+                ?>
+                        <span class="dot" onclick="currentSlide(<?php echo $i; ?>)"></span>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+
+            <script>
+                let slideIndex = 1;
+                showSlides(slideIndex);
+
+                function plusSlides(n) {
+                    showSlides(slideIndex += n);
+                }
+
+                function currentSlide(n) {
+                    showSlides(slideIndex = n);
+                }
+
+                function showSlides(n) {
+                    let i;
+                    let slides = document.getElementsByClassName("mySlides");
+                    let dots = document.getElementsByClassName("dot");
+                    if (n > slides.length) {
+                        slideIndex = 1
+                    }
+                    if (n < 1) {
+                        slideIndex = slides.length
+                    }
+                    for (i = 0; i < slides.length; i++) {
+                        slides[i].style.display = "none";
+                    }
+                    for (i = 0; i < dots.length; i++) {
+                        dots[i].className = dots[i].className.replace(" active", "");
+                    }
+                    slides[slideIndex - 1].style.display = "block";
+                    dots[slideIndex - 1].className += " active";
+                }
+            </script>
+
+            <!-- <div class="row">  
 
                 <div class="col-md-4">
                     <a href="img/L1010290.JPG" class="lightbox_image port-item">
@@ -407,9 +618,9 @@
                         <h2 class="text-center"><a href="img/DSC05537.JPG" class="lightbox_image"> SATURDAY NIGHT LIVE </a></h2>
                         <p class="text-center">Etiam porta sem malesuada magna</p>
                     </div>
-                </div>
-            
-                <!-- <div class="col-md-4">
+                </div> -->
+
+            <!-- <div class="col-md-4">
                     <a href="img/gallery/02.jpg" class="lightbox_image port-item">
                         <span class="hovers"> <i class="icon-plus-circled"></i> </span>
                         <img src="img/gallery/02.jpg" alt="image">
@@ -431,7 +642,7 @@
                         <p class="text-center">Etiam porta sem malesuada magna</p>
                     </div>
                 </div> -->
-           </div>
+        </div>
         </div>
     </section>
     <!-- End Gallery Section -->
